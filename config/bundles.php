fbb78dfe1ea60b592f2664d80cc65508ca0c6eba
Erika <erika.ike@outlook.fr>
2023-08-29 19:01:33 +0200

    swup

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\Dropzone\DropzoneBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Swup\SwupBundle::class => ['all' => true],
 ];
